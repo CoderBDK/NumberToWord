b1a858398868b51b04cd149ce9c3ec9657bbc71a
CoderBdk <cobdgithub07@gmail.com>
2025-05-19 21:44:52 +0600

    Update main function with NumberWordConverter usage

--- numwordconverter/src/main/java/com/coderbdk/numwordconverter/MyClass.kt
+++ numwordconverter/src/main/java/com/coderbdk/numwordconverter/MyClass.kt
@@ -3,7 +3,7 @@ package com.coderbdk.numwordconverter
 fun main() {
     val converter = NumberWordConverter(Type.BANGLA)
 
-    // নয় কোটি নয় লক্ষ নয় হাজার নয় শত নয়
+    // Output: নয় কোটি নয় লক্ষ নয় হাজার নয় শত নয়
     println(converter.numberToWords(90909909))
 
 }
