e491ded4f3f4114761c368cd05f9844f1f36c14f
CoderBdk <cobdgithub07@gmail.com>
2025-05-23 19:11:34 +0600

    Add unit tests for EnglishConverter number-to-word conversion (30, 31, 331)

--- numwordconverter/src/test/java/com/coderbdk/numwordconverter/NumberWordConverterTest.kt
+++ numwordconverter/src/test/java/com/coderbdk/numwordconverter/NumberWordConverterTest.kt
@@ -5,6 +5,7 @@ import org.junit.Test
 
 class NumberWordConverterTest {
     private val banglaConverter = NumberWordConverter(Type.BANGLA)
+    private val englishConverter = NumberWordConverter(Type.ENGLISH)
 
     @Test
     fun numberToWords_bangla_1010() {
@@ -22,5 +23,20 @@ class NumberWordConverterTest {
     fun numberToWords_bangla_11009701() {
         val r = banglaConverter.numberToWords(1_10_09_701)
         assertEquals(r, "এক কোটি দশ লক্ষ নয় হাজার সাত শত এক")
+    }
+    @Test
+    fun numberToWords_english_30() {
+        val r = englishConverter.numberToWords(30)
+        assertEquals(r,"Thirty")
+    }
+    @Test
+    fun numberToWords_english_31() {
+        val r = englishConverter.numberToWords(31)
+        assertEquals(r,"Thirty-One")
+    }
+    @Test
+    fun numberToWords_english_331() {
+        val r = englishConverter.numberToWords(331)
+        assertEquals(r,"Three Hundred Thirty-One")
     }
 }
